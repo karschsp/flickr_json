cefc8f6ab4a08dab0bb8512d4dbad68f36687256
skarsch <skarsch@its.jnj.com>
2019-03-08 16:07:15 -0500

    Remove import running on cron run.

--- src/Plugin/QueueWorker/FlickrImport.php
+++ src/Plugin/QueueWorker/FlickrImport.php
@@ -7,7 +7,6 @@ namespace Drupal\flickr_json\Plugin\QueueWorker;
  * @QueueWorker(
  *   id = "flickr_json",
  *   title = @Translation("Import Content Flickr JSON"),
- *   cron = {"time" = 60}
  * )
  */
 class FlickrImport extends FlickrJsonImport {}
